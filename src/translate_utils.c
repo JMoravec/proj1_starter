d3ef592f7dc443616551989b99479576e7260e9a
Joshua Moravec <joshua.moravec@gmail.com>
2019-04-12 09:44:19 -0500

    Step 1 complete

--- src/translate_utils.c
+++ src/translate_utils.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <errno.h>
 
 #include "translate_utils.h"
 
@@ -38,29 +39,33 @@ int is_valid_label(const char* str) {
     return first ? 0 : 1;   // empty string is invalid
 }
 
-/* Translate the input string into a signed number. The number is then 
+/* Translate the input string into a signed number. The number is then
    checked to be within the correct range (note bounds are INCLUSIVE)
-   ie. NUM is valid if LOWER_BOUND <= NUM <= UPPER_BOUND. 
+   ie. NUM is valid if LOWER_BOUND <= NUM <= UPPER_BOUND.
 
    The input may be in either positive or negative, and be in either
    decimal or hexadecimal format. It is also possible that the input is not
-   a valid number. Fortunately, the library function strtol() can take 
+   a valid number. Fortunately, the library function strtol() can take
    care of all that (with a little bit of work from your side of course).
    Please read the documentation for strtol() carefully. Do not use strtoul()
-   or any other variants. 
+   or any other variants.
 
-   You should store the result into the location that OUTPUT points to. The 
-   function returns 0 if the conversion proceeded without errors, or -1 if an 
+   You should store the result into the location that OUTPUT points to. The
+   function returns 0 if the conversion proceeded without errors, or -1 if an
    error occurred.
  */
-int translate_num(long int* output, const char* str, long int lower_bound, 
+int translate_num(long int* output, const char* str, long int lower_bound,
     long int upper_bound) {
     if (!str || !output) {
         return -1;
     }
-    /* YOUR CODE HERE */
+    long int check_num = strtol(str, NULL, 0);
+    if ((check_num == 0L) || (errno == ERANGE)) {
+        return -1;
+    }
 
-    return -1;
+    *output = check_num;
+    return 0;
 }
 
 /* Translates the register name to the corresponding register number. Please
@@ -73,7 +78,65 @@ int translate_reg(const char* str) {
     else if (strcmp(str, "$0") == 0)    return 0;
     else if (strcmp(str, "$at") == 0)   return 1;
     else if (strcmp(str, "$v0") == 0)   return 2;
+    else if (strcmp(str, "$v1") == 0)   return 3;
     else if (strcmp(str, "$a0") == 0)   return 4;
-    /* YOUR CODE HERE */
-    else                                return 0;
+    else if (strcmp(str, "$a1") == 0)   return 5;
+    else if (strcmp(str, "$a2") == 0)   return 6;
+    else if (strcmp(str, "$a3") == 0)   return 7;
+    else if (strcmp(str, "$t0") == 0)   return 8;
+    else if (strcmp(str, "$t1") == 0)   return 9;
+    else if (strcmp(str, "$t2") == 0)   return 10;
+    else if (strcmp(str, "$t3") == 0)   return 11;
+    else if (strcmp(str, "$t4") == 0)   return 12;
+    else if (strcmp(str, "$t5") == 0)   return 13;
+    else if (strcmp(str, "$t6") == 0)   return 14;
+    else if (strcmp(str, "$t7") == 0)   return 15;
+    else if (strcmp(str, "$s0") == 0)   return 16;
+    else if (strcmp(str, "$s1") == 0)   return 17;
+    else if (strcmp(str, "$s2") == 0)   return 18;
+    else if (strcmp(str, "$s3") == 0)   return 19;
+    else if (strcmp(str, "$s4") == 0)   return 20;
+    else if (strcmp(str, "$s5") == 0)   return 21;
+    else if (strcmp(str, "$s6") == 0)   return 22;
+    else if (strcmp(str, "$s7") == 0)   return 23;
+    else if (strcmp(str, "$t8") == 0)   return 24;
+    else if (strcmp(str, "$t9") == 0)   return 25;
+    else if (strcmp(str, "$k0") == 0)   return 26;
+    else if (strcmp(str, "$k1") == 0)   return 27;
+    else if (strcmp(str, "$gp") == 0)   return 28;
+    else if (strcmp(str, "$sp") == 0)   return 29;
+    else if (strcmp(str, "$fp") == 0)   return 30;
+    else if (strcmp(str, "$ra") == 0)   return 31;
+    else if (strcmp(str, "$1") == 0)    return 1;
+    else if (strcmp(str, "$2") == 0)    return 2;
+    else if (strcmp(str, "$3") == 0)    return 3;
+    else if (strcmp(str, "$4") == 0)    return 4;
+    else if (strcmp(str, "$5") == 0)    return 5;
+    else if (strcmp(str, "$6") == 0)    return 6;
+    else if (strcmp(str, "$7") == 0)    return 7;
+    else if (strcmp(str, "$8") == 0)    return 8;
+    else if (strcmp(str, "$9") == 0)    return 9;
+    else if (strcmp(str, "$10") == 0)    return 10;
+    else if (strcmp(str, "$11") == 0)    return 11;
+    else if (strcmp(str, "$12") == 0)    return 12;
+    else if (strcmp(str, "$13") == 0)    return 13;
+    else if (strcmp(str, "$14") == 0)    return 14;
+    else if (strcmp(str, "$15") == 0)    return 15;
+    else if (strcmp(str, "$16") == 0)    return 16;
+    else if (strcmp(str, "$17") == 0)    return 17;
+    else if (strcmp(str, "$18") == 0)    return 18;
+    else if (strcmp(str, "$19") == 0)    return 19;
+    else if (strcmp(str, "$20") == 0)    return 20;
+    else if (strcmp(str, "$21") == 0)    return 21;
+    else if (strcmp(str, "$22") == 0)    return 22;
+    else if (strcmp(str, "$23") == 0)    return 23;
+    else if (strcmp(str, "$24") == 0)    return 24;
+    else if (strcmp(str, "$25") == 0)    return 25;
+    else if (strcmp(str, "$26") == 0)    return 26;
+    else if (strcmp(str, "$27") == 0)    return 27;
+    else if (strcmp(str, "$28") == 0)    return 28;
+    else if (strcmp(str, "$29") == 0)    return 29;
+    else if (strcmp(str, "$30") == 0)    return 30;
+    else if (strcmp(str, "$31") == 0)    return 31;
+    else                                return -1;
 }
